aec58cac939296b91016b18df83c9eb0a46610a4
Akarsh Kantimath <akarshkantimath@gmail.com>
2024-06-09 22:09:42 +0530

    Sorting the Strings in ascending and descending order

--- 5.SortingArrayString.js
+++ 5.SortingArrayString.js
@@ -3,7 +3,7 @@ let fruits = ["banana", "mango", "apple", "pineapple"] //make sure to give eithe
 fruits.sort()
 console.log(fruits)
 
-//We can print sorted array in a singlw line such as 
+//We can print sorted array in a single line such as 
 console.log(fruits.sort())
 
 //If we want reverse the sorting of an array use reverse()
