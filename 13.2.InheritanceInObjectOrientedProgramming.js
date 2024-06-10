ba8fb9378acf1e6d17a960b1eeae6479d77aee17
Akarsh Kantimath <akarshkantimath@gmail.com>
2024-06-10 20:29:37 +0530

    Inheritance concept in object oriented programming

---
+++ 13.2.InheritanceInObjectOrientedProgramming.js
@@ -0,0 +1,7 @@
+//Main purpose to Inheritance is to avoid duplication of code and extend the same method alredy present the parent class
+//Inheritance is the main pillar in object oriented programming
+//One class can inherit/acquire the properties, methods of another class
+//The class which inherits the properties of other class is known as subclass (derived class/child class)
+//the class whose properties are inherited is known as superclass
+
+//extends key word helps to inherit the parent class
